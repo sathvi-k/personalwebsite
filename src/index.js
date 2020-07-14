ff0a080cbafde2c45146a96881cb4cd821fb1a51
Sathvi <47374355+sathvi-k@users.noreply.github.com>
2020-07-14 16:13:40 -0400

    done!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("starting up!");
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+$('#main').html(`You've been on this page for ${num} second(s).`);
+
+/* https://www.w3schools.com/jsref/met_win_setinterval.asp */
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} second(s).`);
+}, 1000);
